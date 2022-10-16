c79cb09c84ee52b9a42dd55afd83bcb6b1fa367c
Sakthisree <sakthisreem237@gmail.com>
2022-10-16 17:25:30 +0530

    Basic concepts

--- Basic_concepts.py
+++ Basic_concepts.py
@@ -1,1 +1,20 @@
-print("hello")
+#-------------To print----------------
+print("Hello World")
+
+#-------------Simple operations-------
+print(2+4-1)
+
+#-------------Arithmetic operator-----
+print(6+3) #Addition operator
+print(5-2) #Subraction operator
+print(9*2) #Multiplication operator
+print(4/2) #Division whichw will print a decimal number
+print(4//2) #Quotient
+print(5%2) #Remainder
+print(5**2) #Exponential 
+
+#------------Input-------------
+a= input() #Will take string as input
+#For specifi type the type casting can be applied to input
+#eg for integer
+a = int(input())
